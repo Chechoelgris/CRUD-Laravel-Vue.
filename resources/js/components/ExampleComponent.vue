3d22fe1ee10429e2a873c5e39ecb9e3aea25cc58
Chechoelgris <so.sepulveda.m@gmail.com>
2019-08-09 22:48:36 -0400

    Commit del proyecto finalizado

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Componente de ejemplo</div>
+
+                    <div class="card-body">
+                        Este es un componente de ejemplo 100% funcional!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
