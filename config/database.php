787a6912df4f066cb9d6be0307dbb74531a882cd
Chechoelgris <so.sepulveda.m@gmail.com>
2019-08-13 14:09:18 -0400

    configurada base de datos

--- config/database.php
+++ config/database.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL = parse_url('ostgres://fuqjkuklkgsjxl:a9ab625d6f9f593af1067701d841495bb427e5fdf0cb5efe00a4a0f0dacd335a@ec2-54-221-201-212.compute-1.amazonaws.com:5432/d6jkod65b36dl9');
 return [
 
     /*
@@ -46,11 +47,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL['user'],
+            'password' => $DATABASE_URL['pass'],
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
