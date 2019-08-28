1340b664f12c971ec3f70a0709ab111a8f281bd2
Chechoelgris <so.sepulveda.m@gmail.com>
2019-08-28 03:07:13 -0400

    config login controller 7

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
-            $table->string('provider');
-            $table->string('provider_id')->unique();
+            $table->string('provider_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
